3415fda3b3bd635d0b3e64aa0b4f99f75e2fb6a7
xuxiaoqun <2071504720@qq.com>
2020-05-28 13:45:59 +0800

    test

--- src/main/java/com/xzc/swagger/demo/controller/TestController.java
+++ src/main/java/com/xzc/swagger/demo/controller/TestController.java
@@ -1,6 +1,6 @@
 package com.xzc.swagger.demo.controller;
 
-import com.xzc.swagger.demo.api.TestApi;
+import com.google.common.collect.Maps;
 import com.xzc.swagger.demo.param.TestParam;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,27 +8,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * @author xzc
+ */
 @RestController
 @Validated
 public class TestController {
 
     @GetMapping("/hello")
     public Map<String,Object> hello( @NotBlank String name) {
-        Map<String,Object> person = new HashMap(1);
+        Map<String,Object> person = Maps.newHashMapWithExpectedSize(1);
         person.put("name",name);
         return person;
     }
 
     @PostMapping("/hello1")
     public Map<String, Object> hello1(@RequestBody @Validated TestParam param) {
-        Map<String,Object> person = new HashMap<>();
+        Map<String,Object> person = Maps.newHashMapWithExpectedSize(1);
         person.put("name",param.getName());
         person.put("age",param.getAge());
         person.put("sex",param.getSex());
