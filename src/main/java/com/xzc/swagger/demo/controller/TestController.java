5b4b72039d8bf81202a01a7679434f957145b5fd
xuxiaoqun <2071504720@qq.com>
2020-06-13 14:13:59 +0800

    test

--- src/main/java/com/xzc/swagger/demo/controller/TestController.java
+++ src/main/java/com/xzc/swagger/demo/controller/TestController.java
@@ -2,12 +2,15 @@ package com.xzc.swagger.demo.controller;
 
 import com.google.common.collect.Maps;
 import com.xzc.swagger.demo.param.TestParam;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.validation.constraints.NotBlank;
 import java.util.Map;
 
@@ -16,7 +19,19 @@ import java.util.Map;
  */
 @RestController
 @Validated
+@Slf4j
 public class TestController {
+
+    @PostConstruct
+    public void init(){
+        log.info("初始化");
+        System.out.println("初始化");
+    }
+
+    @PreDestroy
+    public void destroy(){
+        log.info("销毁");
+    }
 
     @GetMapping("/hello")
     public Map<String,Object> hello( @NotBlank String name) {
